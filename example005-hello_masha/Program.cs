5dedcc64acc1e14f9060e9c919d4acab63268609
Bartoshyk Olga <eireen@mail.ru>
2023-11-26 20:40:56 +0300

    условие и команда to lower

---
+++ example005-hello_masha/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ура, это же Маша");
+}
+else
+{
+  Console.WriteLine("hello");  
+}
